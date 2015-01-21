c4764a0ba0ff76be2f16cc75b4d73146ebd41441
Gary Nakanelua <gnakan@gmail.com>
2015-01-20 21:21:56 -0800

    all kinds of updates

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ var app = angular.module('pirateTalk', []);
 
 
 app.factory('PirateTalk', function ($http){
+	
 	return {
-		translate: function(textToTranslate){
+		translate: function(textToTranslate, successCallback){
 			var apiURL = "http://isithackday.com/arrpi.php?text=" + textToTranslate + "&format=json&callback=JSON_CALLBACK";
 			$http.jsonp(apiURL)
-			.success(function(response){ console.log(response);return response; });
+			.success(function(response){ 
+				successCallback(response.translation.pirate); //for simplicity in this tutorial, using a callback instead of a promise
+			});
 		}
 	}
-
 });
-
-
 
 
 //Create the controller. For simplicity, including this in one file, rather than separating this out.
@@ -22,7 +22,15 @@ app.controller('PirateController', function($scope, PirateTalk){
 	//cast a controller instance into a variable, naming it 'vm' which stands for ViewModel. This is simply based on style guidelines
 	var vm = this;
 	vm.appTitle = "Talk Like A Pirate";
+	vm.newText = PirateTalk.blah;
+	vm.dataLoading = false;
 
-	PirateTalk.translate('Hello my friend, do you know where I can get a beer');
-
+	$scope.sendText = function(){
+		vm.dataLoading = true;
+		PirateTalk.translate($scope.originalText, function success(data){
+			$scope.originalText = null; //reset the value of the text area field
+			vm.newText = data;
+			vm.dataLoading = false;
+		});
+	};
 });
--- readme.md
+++ readme.md
@@ -1,13 +1,20 @@
 #Overview
-Tutorial application
+Tutorial application to demonstrate some basic concepts for getting started with AngularJS. A few areas covered:
+
+1. Creating a module
+2. Using a factory
+3. Using a controller
+4. Two-way binding from data pulled by the factory
+5. Basic form handling
 
 
 
-Want to hand code it?
+##Want to hand code it?
 Step 1: Download custom build of HTML5 Boilerplate - http://www.initializr.com/. In the build, don't worry about including jQuery or Google Analytics.
 Step 2: Clean up the index html (no need for article or footer)
 Step 3: Add the AngularJS library to your index.html (above the main.js script includes)
+Step 4: Add your AngularJS goodness
 
-
+Using API at http://isithackday.com/arrpi.php
 
 Helpful links available [here](https://gist.github.com/gnakan/19609d6e52ab330de363#file-angularjs-links-md)
